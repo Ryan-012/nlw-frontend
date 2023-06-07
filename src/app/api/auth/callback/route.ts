abc3b6d6135ee7775913f432eb83b4babbcfb706
ryan aragao <ryanaragao012@gmail.com>
2023-06-07 11:21:58 -0300

    vercel upload

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -2,7 +2,6 @@ import { api } from '@/lib/api'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
- 
   const { searchParams } = new URL(req.url)
 
   const code = searchParams.get('code')
