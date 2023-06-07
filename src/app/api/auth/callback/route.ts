a6beaa2ddb55b49a3c09def0aacd8ce7e1b56460
Ryan Aragão <97786299+Ryan-012@users.noreply.github.com>
2023-06-07 10:31:23 -0300

    Update route.ts

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -2,7 +2,8 @@ import { api } from '@/lib/api'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
-  console.log('test')
+  await req.headers.set('')
+  
   const { searchParams } = new URL(req.url)
 
   const code = searchParams.get('code')
