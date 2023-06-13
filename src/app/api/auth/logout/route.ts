721b63019fc2df2e0a25fd549e69816e783f5ff9
ryan aragao <ryanaragao012@gmail.com>
2023-06-13 19:31:49 -0300

    change url

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
-  const redirectURL = new URL('/', req.url)
+  const redirectURL = new URL('/', 'https://main.d3dn7d96quisvt.amplifyapp.com')
 
   return NextResponse.redirect(redirectURL, {
     headers: {
