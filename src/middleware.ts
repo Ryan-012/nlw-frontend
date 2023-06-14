78a483390cf1bfd2d879803689fdc804437ae167
ryan aragao <ryanaragao012@gmail.com>
2023-06-14 12:51:19 -0300

    update  middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 const signInURL = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`
+
 export function middleware(req: NextRequest) {
   const token = req.cookies.get('token')?.value
 
-  if (!token) {
+  if (!token && req.nextUrl.pathname !== '/') {
+    console.log('test')
     return NextResponse.redirect(signInURL, {
       headers: {
-        'Set-Cookie': `redirectTo=${process.env.NEXT_PUBLIC_URL}${req.nextUrl.pathname}; Path=/; HttpOnly; max-age=50`,
+        'Set-Cookie': `redirectTo=${req.url}; Path=/; HttpOnly; max-age=50`,
       },
     })
   }
 
   return NextResponse.next()
 }
-
 export const config = {
-  middleware: async () => {
-    return {
-      matcher: (req: NextRequest) => {
-        if (req.nextUrl.pathname === '/') {
-          return false // Não executar o middleware para a rota raiz
-        }
-
-        return ['/memories/:path*', '/users/:path*'].some((pattern) =>
-          req.nextUrl.pathname.startsWith(pattern),
-        )
-      },
-    }
-  },
+  matcher: ['/memories/:path*', '/users/:path*'],
 }
