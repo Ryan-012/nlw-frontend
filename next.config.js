5e6895e25576bbfeb0a070dbcc17fcadb01002f5
ryan aragao <ryanaragao012@gmail.com>
2023-06-14 19:12:05 -0300

    change next config

--- next.config.js
+++ next.config.js
@@ -6,4 +6,17 @@ module.exports = {
       'nlw-spacetime-project.s3.amazonaws.com',
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Padrão que corresponde a todas as rotas
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // Permite solicitações de qualquer origem
+          },
+        ],
+      },
+    ]
+  },
 }
