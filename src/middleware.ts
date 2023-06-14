5d38d5ed6bd017823920f98b6765dac5f698f16b
ryan aragao <ryanaragao012@gmail.com>
2023-06-14 09:28:03 -0300

    update nextconfig

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 const signInURL = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`
 export function middleware(req: NextRequest) {
   const token = req.cookies.get('token')?.value
-
+  console.log('test')
   if (!token) {
     return NextResponse.redirect(signInURL, {
       headers: {
@@ -12,7 +12,7 @@ export function middleware(req: NextRequest) {
     })
   }
 
-  return NextResponse.next().headers.append('Access-Control-Allow-Origin', '*')
+  return NextResponse.next()
 }
 
 export const config = {
